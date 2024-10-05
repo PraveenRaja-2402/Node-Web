b0cf4b7aec5400de5c20ef0d6cdcb4f9bc2b19c5
praveen <praveen.r2402@gmail.com>
2024-10-05 22:52:50 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,38 +1,91 @@
 const path = require('path')
 const express = require("express")
+const hbs =require('hbs')
 const app = express();
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-console.log(__dirname)
+//Define path for for express engine
 const publicdirectoryPath = path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
+
+//setup handle bar engine and views location
+app.set('view engine','hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
+
+//setup static directory to serve 
 app.use(express.static(publicdirectoryPath))
 
+app.get('', (req, res) => {
+res.render('index',{
+  title : 'Weather app from app.js ',
+  name : 'Praveen Raja'
+})
+})
 
-app.get("", (req, res) => {
-  res.send("<h1>Weather</h1>");
-});
+app.get('/about',(req,res) => {
+  res.render('about',{
+    name : 'Praveen Raja',
+    title : 'About Me'
+  })
+})
 
-app.get("/help", (req, res) => {
-  res.send(
-    {
-      name: "Hinata",
-    },
-    {
-        name : "Sakura"
-    }
-  );
-});
+app.get('/help', (req,res) => {
+  res.render('help',{
+    title : 'help',
+    name  : 'Hobes',
+    message : 'This application is user friendly '
+  })
+})
 
-app.get("/about", (req, res) => {
-  res.send("<h1>About</h1>");
-});
+app.get('/product', (req,res) => {
+  if(!req.query.search){
+       res.send({
+        error : 'You must provide the search term'
+       })
+  }
+
+  console.log(req.query)
+  res.send({
+    product : []
+  })
+})
+
+app.get('/help/*',(req,res) => {
+  res.send('Help artical not found')
+})
 
 app.get("/weather", (req, res) => {
+  if(!req.query.address){
+    return res.send({
+      error : 'You must provide an address'
+    })
+  }
   res.send({
-    forecast: "this is snowing",
-    climate: "cloudy",
-  });
-});
+    forecast: "Extreme Hot Climate",
+    location: "chennai",
+    address : req.query.address
+  })
+})
 
+app.get('/help/*', (req,res) => {
+  res.render('404',{
+    title : '404',    
+    errorMessage : 'Help artical not found!'
+  })
+})
+
+//wild card character
+app.get('*', (req,res) => {
+  res.render('404',{
+    title : '404',
+    errorMessage : 'page not found'
+  })
+})
+
+//port listenner
 app.listen(3000, () => {
   console.log("Server is up on port 3000.");
 });
